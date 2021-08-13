ac8bdf58b68da3f238b785232160ae08924c92d6
Victor Nascimento <nascimento.victor@live.com>
2021-08-13 08:01:04 -0300

    Delete src/modules/Orders directory
    adjusting folder naming

--- src/modules/Orders/repositories/IMongoOrdersRepository.ts
+++
@@ -1,6 +0,0 @@
-import { ICreateDailyOrderDTO } from '../dtos/ICreateOrderDTO';
-
-export interface IMongoOrdersRepository {
-  createDailyDeals(dailyOrders: ICreateDailyOrderDTO): void;
-  findDailyDeals({ date }: { date: string }): {};
-}
